5f975e4e484e5b760535b8ac38874a84a1347583
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-09 22:01:58 +0900

    feat: extract cmd

--- cmd/llctl/llctl.go
+++ cmd/llctl/llctl.go
@@ -1,22 +1,8 @@
 package main
 
-import (
-	"github.com/spf13/cobra"
-)
+import "github.com/yukitaka/longlong/internal/cmd"
 
 func main() {
-	cmd := NewLlctlCommand()
-	cmd.Execute()
-}
-
-func NewLlctlCommand() *cobra.Command {
-	return &cobra.Command{
-		Use:   "llctl",
-		Short: "llctl controls the LongLong manager",
-		Long: `
-llctl controls the LongLong manager.
-
-Find more information at:
-https://github.com/yukitaka/longlong/`,
-	}
+	command := cmd.NewLlctlCommand()
+	command.Execute()
 }
