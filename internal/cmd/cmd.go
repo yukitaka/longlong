fbcc4b1da8b76b365bc30a4f8b54c5e944acd0e7
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-10 19:59:43 +0900

    feat: handling a default help

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,11 +1,37 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"io"
+	"os"
+	"strings"
 )
 
+type LlctlOptions struct {
+	Arguments []string
+	IOStream
+}
+
+type IOStream struct {
+	In     io.Reader
+	Out    io.Writer
+	ErrOut io.Writer
+}
+
 func NewLlctlCommand() *cobra.Command {
-	return &cobra.Command{
+	return NewLlctlCommandWithArgs(LlctlOptions{
+		Arguments: os.Args,
+		IOStream: IOStream{
+			In:     os.Stdin,
+			Out:    os.Stdout,
+			ErrOut: os.Stderr,
+		},
+	})
+}
+
+func NewLlctlCommandWithArgs(o LlctlOptions) *cobra.Command {
+	cmds := &cobra.Command{
 		Use:   "llctl",
 		Short: "llctl controls the LongLong manager",
 		Long: `
@@ -14,4 +40,23 @@ llctl controls the LongLong manager.
 Find more information at:
 https://github.com/yukitaka/longlong/`,
 	}
+
+	if len(o.Arguments) > 1 {
+		cmdArgs := o.Arguments[1:]
+		var cmdName string
+		for _, arg := range cmdArgs {
+			if !strings.HasPrefix(arg, "-") {
+				cmdName = arg
+				break
+			}
+		}
+		switch cmdName {
+		case "help", cobra.ShellCompRequestCmd, cobra.ShellCompNoDescRequestCmd:
+		default:
+			fmt.Fprintf(o.IOStream.ErrOut, "Error: %v\n", cmdName)
+			os.Exit(1)
+		}
+	}
+
+	return cmds
 }
