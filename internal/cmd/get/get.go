f90334bbfe5544d05977efe4fcdd9d17bc41ef14
Takayuki Sato <s.yukkiy@gmail.com>
2023-03-07 22:01:50 +0900

    feat: add IOStream

--- internal/cmd/get/get.go
+++ internal/cmd/get/get.go
@@ -4,23 +4,29 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/yukitaka/longlong/internal/cli"
 )
 
 type GetOptions struct {
 	CmdParent string
+	cli.IOStream
 }
 
-func NewGetOptions(parent string) *GetOptions {
+func NewGetOptions(parent string, streams cli.IOStream) *GetOptions {
 	return &GetOptions{
 		CmdParent: parent,
+		IOStream:  streams,
 	}
 }
 
-func NewCmdGet(parent string) *cobra.Command {
+func NewCmdGet(parent string, streams cli.IOStream) *cobra.Command {
+	_ = NewGetOptions(parent, streams)
+
 	cmd := &cobra.Command{
 		Use:   "get",
 		Short: "Display one or many resources",
 		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("in run")
 			fmt.Println(args)
 		},
 	}
