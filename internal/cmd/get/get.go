3ce65fb113f042a927a2f090ed96c2c779fe2c4e
Takayuki Sato <s.yukkiy@gmail.com>
2023-03-20 01:58:18 +0900

    feat: add table view

--- internal/cmd/get/get.go
+++ internal/cmd/get/get.go
@@ -2,10 +2,15 @@ package get
 
 import (
 	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/yukitaka/longlong/internal/domain/entity"
 	"github.com/yukitaka/longlong/internal/domain/usecase"
 	"github.com/yukitaka/longlong/internal/interface/repository"
+	"os"
 	"strconv"
 
+	"github.com/charmbracelet/bubbles/table"
 	"github.com/spf13/cobra"
 	"github.com/yukitaka/longlong/internal/cli"
 	"github.com/yukitaka/longlong/internal/util"
@@ -80,12 +85,62 @@ func (o *Options) Organization(cmd *cobra.Command, args []string) error {
 	return err
 }
 
+var baseStyle = lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	m.table, cmd = m.table.Update(msg)
+
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
 func (o *Options) print(output string, data interface{}) {
 	if output == "yaml" {
 		if organizationsYaml, err := yaml.Marshal(&data); err == nil {
 			fmt.Println(string(organizationsYaml))
 		}
 	} else {
-		fmt.Printf("%v\n", data)
+		columns := []table.Column{
+			{Title: "PID", Width: 4},
+			{Title: "ID", Width: 4},
+			{Title: "Name", Width: 16},
+		}
+		var rows []table.Row
+		if o, ok := data.(*entity.Organization); ok {
+			rows = append(rows, table.Row{strconv.FormatInt(o.ParentID, 10), strconv.FormatInt(o.ID, 10), o.Name})
+		} else if orgs, ok := data.(*[]entity.Organization); ok {
+			for _, o := range *orgs {
+				rows = append(rows, table.Row{strconv.FormatInt(o.ParentID, 10), strconv.FormatInt(o.ID, 10), o.Name})
+			}
+		}
+		t := table.New(
+			table.WithColumns(columns),
+			table.WithRows(rows),
+			table.WithHeight(len(rows)),
+		)
+
+		m := model{t}
+		if _, err := tea.NewProgram(m).Run(); err != nil {
+			fmt.Println("Error running program: ", err)
+			os.Exit(1)
+		}
 	}
 }
