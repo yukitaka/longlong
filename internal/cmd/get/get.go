4c4872348abcb7bdf18adb823a40a59bd78538f0
Takayuki Sato <s.yukkiy@gmail.com>
2023-03-21 00:31:02 +0900

    feat: add to push esc

--- internal/cmd/get/get.go
+++ internal/cmd/get/get.go
@@ -96,7 +96,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "ctrl+c":
+		case "esc", "q", "ctrl+c":
 			return m, tea.Quit
 		}
 	}
