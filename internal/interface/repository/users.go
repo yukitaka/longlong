35d2a6cc9cacc763caf2067e5e25d3c3f0692107
Takayuki Sato <s.yukkiy@gmail.com>
2023-03-26 23:55:59 +0900

    feat: implement to create user

---
+++ internal/interface/repository/users.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"database/sql"
+	"github.com/yukitaka/longlong/internal/domain/entity"
+	rep "github.com/yukitaka/longlong/internal/domain/repository"
+	"github.com/yukitaka/longlong/internal/util"
+)
+
+type Users struct {
+	users map[int]*entity.User
+	sql.DB
+}
+
+func NewUsersRepository() rep.Users {
+	con, err := sql.Open("sqlite3", "./longlong.db")
+	if err != nil {
+		util.CheckErr(err)
+	}
+
+	return &Users{
+		users: make(map[int]*entity.User),
+		DB:    *con,
+	}
+}
+
+func (rep *Users) Close() {
+	rep.DB.Close()
+}
+
+func (rep *Users) Create(name string) (int64, error) {
+	query := "select max(id) from users"
+	row := rep.DB.QueryRow(query)
+	var nullableId sql.NullInt64
+	err := row.Scan(&nullableId)
+	if err != nil {
+		return -1, err
+	}
+	id := int64(0)
+	if nullableId.Valid {
+		id = nullableId.Int64
+		id++
+	}
+
+	tx, err := rep.DB.Begin()
+	if err != nil {
+		return -1, err
+	}
+	query = "insert into users (id) values (?)"
+	_, err = rep.DB.Exec(query, id, name)
+	if err != nil {
+		return -1, err
+	}
+	query = "insert into profiles (id, name) values (?, ?)"
+	_, err = rep.DB.Exec(query, id, name)
+	if err != nil {
+		return -1, err
+	}
+	tx.Commit()
+
+	return id, nil
+}
+
+func (rep *Users) Find(id int64) (*entity.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
