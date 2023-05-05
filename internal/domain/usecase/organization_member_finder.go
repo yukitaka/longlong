ac60900d28989253e40a44726bcdb3ae9ca88791
Takayuki Sato <s.yukkiy@gmail.com>
2023-05-05 23:23:08 +0900

    feat: implement to find a member

---
+++ internal/domain/usecase/organization_member_finder.go
@@ -0,0 +1,18 @@
+package usecase
+
+import (
+	"github.com/yukitaka/longlong/internal/domain/entity"
+	"github.com/yukitaka/longlong/internal/domain/repository"
+)
+
+type OrganizationMemberFinder struct {
+	repository.OrganizationMembers
+}
+
+func NewOrganizationMemberFinder(members repository.OrganizationMembers) *OrganizationMemberFinder {
+	return &OrganizationMemberFinder{OrganizationMembers: members}
+}
+
+func (it *OrganizationMemberFinder) FindById(organizationId, individualId int64) (*entity.OrganizationMember, error) {
+	return it.OrganizationMembers.Find(organizationId, individualId)
+}
