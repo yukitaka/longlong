fa1048a28e480d5c8186d768782ec8ddac62f684
Takayuki Sato <s.yukkiy@gmail.com>
2023-05-14 00:11:01 +0900

    feat: improve to get user list

--- internal/domain/usecase/user_assigned_test.go
+++ internal/domain/usecase/user_assigned_test.go
@@ -14,19 +14,20 @@ func TestNewUserAssigned(t *testing.T) {
 	defer ctrl.Finish()
 
 	organ := entity.NewOrganization(0, 1, "Test")
-	individuals := &[]entity.Individual{*entity.NewIndividual(1, 1, 1, "Test")}
-	members := &[]entity.OrganizationMember{*entity.NewOrganizationMember(organ, &(*individuals)[0], value_object.MEMBER)}
+	individuals := []entity.Individual{*entity.NewIndividual(1, 1, 1, "Test")}
+	member := entity.NewOrganizationMember(organ, &individuals[0], value_object.MEMBER)
+	members := []entity.OrganizationMember{*member}
 
 	individualRep := mockRepository.NewMockIndividuals(ctrl)
 	organizationRep := mockRepository.NewMockOrganizations(ctrl)
 	memberRep := mockRepository.NewMockOrganizationMembers(ctrl)
 
-	individualRep.EXPECT().FindByUserId(1).Return(individuals, nil)
-	memberRep.EXPECT().IndividualsAssigned(individuals).Return(members, nil)
+	individualRep.EXPECT().FindByUserId(1).Return(&individuals, nil)
+	memberRep.EXPECT().IndividualsAssigned(&individuals).Return(&members, nil)
 	organizationRep.EXPECT().FindAll(gomock.Any()).Return(&[]entity.Organization{*organ}, nil)
 
-	itr := NewUserAssigned(1, individualRep, organizationRep, memberRep)
-	organizations, _ := itr.OrganizationList()
+	itr := NewUserAssigned(individualRep, organizationRep, memberRep)
+	organizations, _ := itr.OrganizationList(member)
 	if organizations == nil {
 		t.Error("Organization List is nil\n")
 	}
