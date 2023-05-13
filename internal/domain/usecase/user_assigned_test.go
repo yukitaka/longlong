f3e1374ad23d67f3074ff9c91e4f4f0b5fe26129
Takayuki Sato <s.yukkiy@gmail.com>
2023-05-13 23:49:51 +0900

    feat: implement to test of user assigned

---
+++ internal/domain/usecase/user_assigned_test.go
@@ -0,0 +1,33 @@
+package usecase
+
+import (
+	"github.com/yukitaka/longlong/internal/domain/entity"
+	"github.com/yukitaka/longlong/internal/domain/value_object"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	mockRepository "github.com/yukitaka/longlong/mock/repository"
+)
+
+func TestNewUserAssigned(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	organ := entity.NewOrganization(0, 1, "Test")
+	individuals := &[]entity.Individual{*entity.NewIndividual(1, 1, 1, "Test")}
+	members := &[]entity.OrganizationMember{*entity.NewOrganizationMember(organ, &(*individuals)[0], value_object.MEMBER)}
+
+	individualRep := mockRepository.NewMockIndividuals(ctrl)
+	organizationRep := mockRepository.NewMockOrganizations(ctrl)
+	memberRep := mockRepository.NewMockOrganizationMembers(ctrl)
+
+	individualRep.EXPECT().FindByUserId(1).Return(individuals, nil)
+	memberRep.EXPECT().IndividualsAssigned(individuals).Return(members, nil)
+	organizationRep.EXPECT().FindAll(gomock.Any()).Return(&[]entity.Organization{*organ}, nil)
+
+	itr := NewUserAssigned(1, individualRep, organizationRep, memberRep)
+	organizations, _ := itr.OrganizationList()
+	if organizations == nil {
+		t.Error("Organization List is nil\n")
+	}
+}
