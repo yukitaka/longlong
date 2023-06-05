cb2b1ca726bd395f2906d8cee926f34cca7c5620
Takayuki Sato <s.yukkiy@gmail.com>
2023-06-05 23:46:39 +0900

    feat: support to create a organization

--- internal/interface/repository/organizations.go
+++ internal/interface/repository/organizations.go
@@ -45,13 +45,13 @@ func (o *Organizations) Create(name string, individual entity.Individual) (int, 
 		id++
 	}
 
-	query = "insert into organizations (id, name) values (?, ?)"
+	query = "insert into organizations (id, name) values ($1, $2)"
 	_, err = o.DB.Exec(query, id, name)
 	if err != nil {
 		return -1, err
 	}
-	query = "insert into organization_members (organization_id, individual_id, role) values (?, ?, ?)"
-	_, err = o.DB.Exec(query, id, name, individual.Id, value_object.OWNER)
+	query = "insert into organization_members (organization_id, individual_id, role) values ($1, $2, $3)"
+	_, err = o.DB.Exec(query, id, individual.Id, value_object.OWNER)
 	if err != nil {
 		return -1, err
 	}
