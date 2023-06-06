ed10fbf155e685994ded5f0c2072d49012aa0134
Takayuki Sato <s.yukkiy@gmail.com>
2023-06-07 00:39:10 +0900

    feat: add a test of a schedule

--- internal/domain/entity/schedule.go
+++ internal/domain/entity/schedule.go
@@ -1,18 +1,73 @@
 package entity
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Schedule struct {
-	Month           int
+	Month           []int
 	MonthInterval   int
-	Day             int
+	Day             []int
 	DayInterval     int
-	Hour            int
+	Hour            []int
 	HourInterval    int
-	Minute          int
+	Minute          []int
 	MinuteInterval  int
-	Weekday         string
+	Weekday         []int
 	WeekdayInterval int
 }
 
 func NewScheduleByCron(cron string) *Schedule {
+	s := &Schedule{}
+	parts := strings.Split(cron, " ")
+	for i := 0; i < len(parts); i++ {
+		numbers, interval := splitNumbersAndInterval(parts[i])
+		switch i {
+		case 0:
+			s.Minute = numbers
+			s.MinuteInterval = interval
+		case 1:
+			s.Hour = numbers
+			s.HourInterval = interval
+		case 2:
+			s.Day = numbers
+			s.DayInterval = interval
+		case 3:
+			s.Month = numbers
+			s.MonthInterval = interval
+		case 4:
+			s.Weekday = numbers
+			s.WeekdayInterval = interval
+		}
+	}
+
 	return &Schedule{}
+}
+
+func splitNumbersAndInterval(s string) ([]int, int) {
+	interval := 1
+	parts := strings.Split(s, "/")
+	if len(parts) == 2 {
+		num, err := strconv.Atoi(parts[1])
+		if err != nil {
+			panic(err)
+		}
+		interval = num
+	}
+	numbers := strings.Split(parts[0], ",")
+	ret := make([]int, len(numbers))
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] == "*" {
+			ret[i] = 0
+		} else {
+			num, err := strconv.Atoi(numbers[i])
+			if err != nil {
+				panic(err)
+			}
+			ret[i] = num
+		}
+	}
+
+	return ret, interval
 }
