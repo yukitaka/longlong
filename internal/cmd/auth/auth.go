e7e8d1456b5c34ccfd7627d0ac6b6c768fc2f61a
Takayuki Sato <s.yukkiy@gmail.com>
2023-06-25 23:49:52 +0900

    feat: extract oauth to interface from cmd

--- internal/cmd/auth/auth.go
+++ internal/cmd/auth/auth.go
@@ -1,24 +1,12 @@
 package auth
 
 import (
-	"context"
-	"crypto/tls"
-	"encoding/json"
-	"fmt"
 	"github.com/jmoiron/sqlx"
-	"github.com/joho/godotenv"
-	"github.com/skratchdot/open-golang/open"
 	"github.com/spf13/cobra"
 	"github.com/yukitaka/longlong/internal/cli"
-	"github.com/yukitaka/longlong/internal/domain/usecase"
-	"github.com/yukitaka/longlong/internal/interface/repository"
+	"github.com/yukitaka/longlong/internal/interface/authentication"
 	"github.com/yukitaka/longlong/internal/util"
-	"golang.org/x/oauth2"
-	"io"
 	"log"
-	"net/http"
-	"os"
-	"time"
 )
 
 type Options struct {
@@ -71,130 +59,13 @@ func (o *Options) Run(args []string) error {
 	return nil
 }
 
-var (
-	mux              = http.NewServeMux()
-	srv              = &http.Server{Addr: ":9999", Handler: mux}
-	ctx              = context.Background()
-	procCtx, procCxl = context.WithTimeout(ctx, 3*time.Second)
-	conf             *oauth2.Config
-)
-
-func (o *authData) callbackOAuth(w http.ResponseWriter, r *http.Request) {
-	code := r.URL.Query().Get("code")
-	log.Printf("Code: %s\n", code)
-
-	oauthToken, err := conf.Exchange(ctx, code)
-	o.token <- oauthToken.AccessToken
-	if err != nil {
-		panic(err)
-	}
-	client := conf.Client(ctx, oauthToken)
-	res, err := client.Get("https://api.github.com/user")
-	if err != nil {
-		panic(err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(res.Body)
-	jsonBody := make(map[string]interface{})
-	_ = json.NewDecoder(res.Body).Decode(&jsonBody)
-	o.login <- jsonBody["login"].(string)
-
-	var (
-		shutdownCtx, shutdownCxl = context.WithTimeout(ctx, 1*time.Second)
-	)
-	defer shutdownCxl()
-
-	if err := srv.Shutdown(shutdownCtx); err != nil {
-		switch err {
-		case context.DeadlineExceeded:
-			log.Println("Sever shutdown timeout")
-		default:
-			log.Println(err)
-		}
-
-	}
-	log.Println("Sever has been shutdown")
-}
-
-func (d *authData) auth(db *sqlx.DB) {
-	var login, token string
-L:
-	for {
-		select {
-		case login = <-d.login:
-		case token = <-d.token:
-		}
-		if login != "" && token != "" {
-			break L
-		}
-	}
-
-	authRep := repository.NewAuthenticationsRepository(db)
-	organizationRep := repository.NewOrganizationsRepository(db)
-	memberRep := repository.NewOrganizationMembersRepository(db)
-	rep := usecase.NewAuthenticationRepository(authRep, organizationRep, memberRep)
-	defer rep.Close()
-
-	itr := usecase.NewAuthentication(rep)
-
-	id, err := itr.AuthOAuth(login, token)
-	if err != nil {
-		return
-	}
-	fmt.Println()
-	log.Printf("Login %s %s %d.\n", login, token, id)
-}
-
-type authData struct {
-	login chan string
-	token chan string
-}
-
 func (o *Options) Login(args []string) error {
-	defer procCxl()
-
-	_ = godotenv.Load(".env")
 	log.Println("Start login.")
-	clientID := os.Getenv("CLIENT_ID")
-	clientSecret := os.Getenv("CLIENT_SECRET")
-
-	conf = &oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		Scopes:       []string{"openid", "user"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://github.com/login/oauth/authorize",
-			TokenURL: "https://github.com/login/oauth/access_token",
-		},
-		RedirectURL: "http://localhost:9999",
+	oauth := authentication.NewOAuth()
+	err := oauth.Run(o.DB)
+	if err != nil {
+		return err
 	}
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	sslcli := &http.Client{Transport: tr}
-	ctx = context.WithValue(ctx, oauth2.HTTPClient, sslcli)
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-
-	fmt.Println("You will now be taken to your browser for authentication")
-	time.Sleep(1 * time.Second)
-	open.Run(url)
-	time.Sleep(1 * time.Second)
-	fmt.Printf("Authentication URL: %s\n", url)
-
-	passer := &authData{login: make(chan (string)), token: make(chan (string))}
-	mux.HandleFunc("/", passer.callbackOAuth)
-	go func() {
-		err := srv.ListenAndServe()
-		if err != nil {
-			return
-		}
-	}()
-	passer.auth(o.DB)
-	<-procCtx.Done()
 
 	return nil
 }
---
+++ internal/interface/authentication/oauth.go
@@ -0,0 +1,155 @@
+package authentication
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	"github.com/skratchdot/open-golang/open"
+	"github.com/yukitaka/longlong/internal/domain/usecase"
+	"github.com/yukitaka/longlong/internal/interface/repository"
+	"golang.org/x/oauth2"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	mux              = http.NewServeMux()
+	srv              = &http.Server{Addr: ":9999", Handler: mux}
+	ctx              = context.Background()
+	procCtx, procCxl = context.WithTimeout(ctx, 3*time.Second)
+	conf             *oauth2.Config
+)
+
+type OAuth struct {
+	login chan string
+	token chan string
+}
+
+func NewOAuth() *OAuth {
+	return &OAuth{
+		login: make(chan string),
+		token: make(chan string),
+	}
+}
+
+func (o *OAuth) Run(db *sqlx.DB) error {
+	defer procCxl()
+
+	_ = godotenv.Load(".env")
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+
+	conf = &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       []string{"openid", "user"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://github.com/login/oauth/authorize",
+			TokenURL: "https://github.com/login/oauth/access_token",
+		},
+		RedirectURL: "http://localhost:9999",
+	}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, client)
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+
+	fmt.Println("You will now be taken to your browser for authentication")
+	time.Sleep(1 * time.Second)
+	err := open.Run(url)
+	if err != nil {
+		return err
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Printf("Authentication URL: %s\n", url)
+
+	mux.HandleFunc("/", o.callbackOAuth)
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			return
+		}
+	}()
+	o.auth(db)
+	<-procCtx.Done()
+
+	return nil
+}
+
+func (o *OAuth) callbackOAuth(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	log.Printf("Code: %s\n", code)
+
+	oauthToken, err := conf.Exchange(ctx, code)
+	o.token <- oauthToken.AccessToken
+	if err != nil {
+		panic(err)
+	}
+	client := conf.Client(ctx, oauthToken)
+	res, err := client.Get("https://api.github.com/user")
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(res.Body)
+	jsonBody := make(map[string]interface{})
+	_ = json.NewDecoder(res.Body).Decode(&jsonBody)
+	o.login <- jsonBody["login"].(string)
+
+	var (
+		shutdownCtx, shutdownCxl = context.WithTimeout(ctx, 1*time.Second)
+	)
+	defer shutdownCxl()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		switch err {
+		case context.DeadlineExceeded:
+			log.Println("Sever shutdown timeout")
+		default:
+			log.Println(err)
+		}
+
+	}
+	log.Println("Sever has been shutdown")
+}
+
+func (d *OAuth) auth(db *sqlx.DB) {
+	var login, token string
+L:
+	for {
+		select {
+		case login = <-d.login:
+		case token = <-d.token:
+		}
+		if login != "" && token != "" {
+			break L
+		}
+	}
+
+	authRep := repository.NewAuthenticationsRepository(db)
+	organizationRep := repository.NewOrganizationsRepository(db)
+	memberRep := repository.NewOrganizationMembersRepository(db)
+	rep := usecase.NewAuthenticationRepository(authRep, organizationRep, memberRep)
+	defer rep.Close()
+
+	itr := usecase.NewAuthentication(rep)
+
+	id, err := itr.AuthOAuth(login, token)
+	if err != nil {
+		return
+	}
+	fmt.Println()
+	log.Printf("Login %s %s %d.\n", login, token, id)
+}
