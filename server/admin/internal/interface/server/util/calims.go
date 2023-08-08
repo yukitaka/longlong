c2f1fa2f21f75e5505d0d28048bdde536e9d62ca
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-08 23:42:45 +0900

    feat: find a operator

--- server/admin/internal/interface/server/util/calims.go
+++ server/admin/internal/interface/server/util/calims.go
@@ -4,6 +4,9 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	serverjwt "github.com/yukitaka/longlong/server/admin/internal/interface/server/jwt"
+	"github.com/yukitaka/longlong/server/core/pkg/domain/entity"
+	"github.com/yukitaka/longlong/server/core/pkg/domain/usecase"
+	"github.com/yukitaka/longlong/server/core/pkg/interface/datastore"
 )
 
 func UserData(c echo.Context) (individualId, organizationId int) {
@@ -11,4 +14,15 @@ func UserData(c echo.Context) (individualId, organizationId int) {
 	claims := user.Claims.(*serverjwt.CustomClaims)
 
 	return claims.IndividualId, claims.OrganizationId
+}
+
+func Operator(c echo.Context) *entity.OrganizationMember {
+	individualId, organizationId := UserData(c)
+	con := c.Get("datastore").(*datastore.Connection)
+	member, err := usecase.NewOrganizationMemberFinder(con).FindById(organizationId, individualId)
+	if err != nil {
+		panic("Can't Find a operator")
+	}
+
+	return member
 }
